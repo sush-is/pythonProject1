8994fc049b88d11145b6d5397ba65e55480ed9fc
sush-is <sushmamodagekar16@gmail.com>
2024-08-10 17:17:39 +0530

    print max number

--- src/ex_08082024/lab004.py
+++ src/ex_08082024/lab004.py
@@ -1,3 +1,3 @@
-#program to find the max number between two integers
-#34,100->100
-print(max(34,100,101))
+# program to find the max number between two integers
+# 34,100->100
+print(max(34, 100, 101))
